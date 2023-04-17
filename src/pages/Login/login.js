837c01b414cbb7c55026d38936a21683a590fa7f
chenwenxue <chenwenxue@winshang.com>
2023-04-17 17:13:20 +0800

    feat: 接入登陆逻辑

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,22 +1,48 @@
+import React, { useState } from 'react';
 import styles from './login.module.css'
+import { Input, Message,Form,Button } from '@arco-design/web-react';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  const handleLogin = ( ) => {
+    console.log('username',username,'password',password)
+    // 模拟登录验证过程，实际项目中，你需要调用后端 API 进行验证
+    if (username === 'admin' && password === 'admin') {
+      setIsLoggedIn(true);
+      Message.success('登陆成功！')
+    } else {
+      Message.error('账号或者密码不存在！')
+    }
+  };
+
+  const handleUsernameChange = (e) => {
+    // console.log('event',event)
+    setUsername(e)
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e)
+  };
+
   return (
     <div class={styles.loginContainer}>
       <h2>欢迎使用</h2>
-      <form class={styles.loginForm}>
+      <Form class={styles.loginForm}>
         <div class={styles.inputGroup}>
-          <label for={styles.username}>用户名</label>
-          <input type="text" id="username" name="username" required />
+          <label>用户名</label>
+          <Input value={username} onChange={handleUsernameChange} allowClear placeholder='请输入用户名' />
         </div>
         <div class={styles.inputGroup}>
-          <label for="password">密码</label>
-          <input type="password" id="password" name="password" required />
+          <label>密码</label>
+          <Input.Password  value={password} onChange={handlePasswordChange} allowClear placeholder='请输入密码' />
         </div>
-        <button type="submit">登陆</button>
-      </form>
+        <Button type='primary' onClick={handleLogin}>登陆</Button >
+      </Form>
     </div>
-  );
+  )
 }
 
 export default Login;
