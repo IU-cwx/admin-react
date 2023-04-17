abc1a6e69b27322b51b61fdb57fa939db1696d20
chenwenxue <chenwenxue@winshang.com>
2023-04-17 14:18:57 +0800

    feat: 新增登陆页

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,11 +1,22 @@
+import styles from './login.module.css'
 
-
-function Login(){
+function Login() {
   return (
-    <div>
-      登陆页
+    <div class={styles.loginContainer}>
+      <h2>欢迎使用</h2>
+      <form class={styles.loginForm}>
+        <div class={styles.inputGroup}>
+          <label for={styles.username}>用户名</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div class={styles.inputGroup}>
+          <label for="password">密码</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">登陆</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Login;
